84277fc8ea1e0087bfd35f128640234b01030e0f
jackson.oliveira <jackson.oliveira@ilegra.com>
2022-06-16 08:27:20 -0300

    updating gitignore

---
+++ customer-balance-aggregations/src/main/java/io/joliveira/TopologyBuilder.java
@@ -0,0 +1,2 @@
+package io.joliveira;public class TopologyBuilder {
+}
---
+++ customer-balance-aggregations/src/test/java/io/joliveira/TopologyBuilderTest.java
@@ -0,0 +1,20 @@
+package io.joliveira;
+
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class StreamingAppTest {
+    private TopologyTestDriver testDriver;
+
+    @BeforeAll
+    public void setup() {
+
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+}
